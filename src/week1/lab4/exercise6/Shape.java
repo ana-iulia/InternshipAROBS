e8b190dbb5970c14ee8957e182a9ca58787f85b8
ana-iulia <eair2712@scs.ubbcluj.ro>
2022-03-09 14:23:07 +0200

    lab4

---
+++ src/week1/lab4/exercise6/Shape.java
@@ -0,0 +1,43 @@
+package week1.lab4.exercise6;
+
+public class Shape {
+    private String color;
+    private boolean filled;
+
+    public Shape() {
+        this.color = "green";
+        this.filled = true;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        String isFilled;
+        if (filled) {
+            isFilled = "filled";
+        } else {
+            isFilled = "not filled";
+        }
+        return "A Shape with color of " + color + " and " + isFilled;
+    }
+}
